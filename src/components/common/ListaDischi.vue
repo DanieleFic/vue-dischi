be4448184296e4cf79737ab1489e30ac5ed30caf
DanieleFic <reb_ficus22@hotmail.it>
2022-01-31 15:55:36 +0100

    da fare bonus

--- src/components/common/ListaDischi.vue
+++ src/components/common/ListaDischi.vue
@@ -1,16 +1,47 @@
 <template>
-  <div class="ms_disco">
-
+  <div class="ms_boxdisco">
+      <img :src="info.poster" alt="">
+      <h4>{{info.title}}</h4>
+      <p>{{info.author}}</p>
+      <p>{{info.year}}</p>
   </div>
 </template>
 
 <script>
 export default {
   name: 'ListaDischi',
-  
+  props: {
+        info: Object
+    }
 }
 </script>
 
-<style>
+<style  lang="scss" scoped>
 @import "../../assets/style/vars.scss";
+
+
+
+.ms_boxdisco{
+  color: white;
+  text-align: center;
+  height: 350px;
+  background-color: $headercolor;
+  margin: 20px 20px;
+
+  img{
+    width: 180px;
+    padding: 10px 0px;
+  }
+
+  h4{
+    padding-bottom: 15px;
+  }
+
+  p{
+    margin: 0;
+    color: $text-color;
+    font-size: 13px;
+  }
+
+}
 </style>
--- src/components/macro/Main.vue
+++ src/components/macro/Main.vue
@@ -2,7 +2,11 @@
   <div class="ms_main">
     <div class="container">
         <div class="row">
-          <ListaDischi/>
+          <ListaDischi
+          v-for="(element,index) in dischiArray"
+          :key="index"
+          :info="element"
+          class=" col-12 col-sm-6 col-lg-2"/>
         </div>
     </div>
   </div>
@@ -10,14 +14,42 @@
 </template>
 
 <script>
-import ListaDischi from "../common/ListaDischi.vue" 
+import axios from "axios";
+import ListaDischi from "../common/ListaDischi.vue"
 export default {
   name: 'Main',
-components: {
+  components: {
     ListaDischi,
-  
-  }
-  }
+  },
+  data() {
+        return {
+            apiURL: "https://flynn.boolean.careers/exercises/api/array/music",
+            dischiArray: [],
+            loading: true
+        }
+    },
+    created(){
+        this.getAlbum();
+        //console.log(this.dischiArray)
+    },
+    methods: {
+        getAlbum(){
+            axios
+                .get(this.apiURL)
+                .then( (risposta) => {
+                    // handle success
+                    this.dischiArray = risposta.data.response;
+                    console.log(this.dischiArray)
+                    
+                    
+                })
+                .catch(function (error) {
+                    // handle error
+                    console.log(error);
+                });
+        }
+    }
+}
 </script>
 
 <style scoped lang="scss">
@@ -26,5 +58,9 @@ components: {
     .ms_main{
         height: calc(100vh - 45px);
         background-color: $maincolor;
+
+        .container{
+          padding: 50px 0px;
+        }
     }
 </style>

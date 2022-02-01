da49d4112e2a59e806723ba5b8af696c6b2feeec
DanieleFic <reb_ficus22@hotmail.it>
2022-02-01 17:50:38 +0100

    senza bonus con difficoltà

---
+++ src/components/common/_Input.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="box">
+    <select v-model="filtra" @change="$emit('cerca', filtra)">
+        <option value="Null">Seleziona</option>
+        <option value="Rock">Rock</option>
+        <option value="Jazz">Jazz</option>
+        <option value="Pop">Pop</option>
+        <option value="Metal">Metal</option>
+    </select>
+</div>
+</template>
+
+<script>
+export default {
+    name:"Input",
+    data(){
+        return{
+            filtra:""
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+    @import "../../assets/style/vars.scss";
+
+
+    select {
+    background-color: $headercolor;
+    color: white;
+    padding: 12px;
+    width: 250px;
+    border: none;
+    font-size: 20px;
+    box-shadow: 0 5px 25px rgba(0, 0, 0, 0.2);
+    outline: none;
+
+}
+
+.box select option {
+    padding: 30px;
+}
+</style>
--- src/components/macro/Main.vue
+++ src/components/macro/Main.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="ms_main">
     <div class="container">
+      <Input
+      @cerca="cambioValore"
+      />
         <div v-if="!loading" class="row">
           <ListaDischi
-          v-for="(element,index) in dischiArray"
+          v-for="(element,index) in filtraGeneri"
           :key="index"
           :info="element"
           class=" col-12 col-sm-6 col-lg-2"/>
@@ -18,24 +21,38 @@
 import axios from "axios";
 import ListaDischi from "../common/ListaDischi.vue"
 import Loader from "../common/Loading.vue"
+import Input from "../common/_Input.vue"
 
 export default {
   name: 'Main',
   components: {
     ListaDischi,
-    Loader
+    Loader,
+    Input
   },
   data() {
         return {
             apiURL: "https://flynn.boolean.careers/exercises/api/array/music",
             dischiArray: [],
-            loading: true
+            loading: true,
+            valore:""
         }
     },
     created(){
         this.getAlbum();
         //console.log(this.dischiArray)
     },
+    computed: {
+        filtraGeneri(){
+          if(this.valore == "Null") return this.dischiArray
+          
+            return this.dischiArray.filter( (album) => {
+                console.log('filtra album');
+                return album.genre.includes(this.valore);
+            });
+        }
+    },
+    
     methods: {
         getAlbum(){
             axios
@@ -51,9 +68,18 @@ export default {
                     // handle error
                     console.log(error);
                 });
-        }
+        },
+        cambioValore(event){
+            this.valore = event;
+            console.log(this.valore);  
+          }
+        },
+        /* valueSelect(){
+
+        } */
     }
-}
+    
+
 </script>
 
 <style scoped lang="scss">
